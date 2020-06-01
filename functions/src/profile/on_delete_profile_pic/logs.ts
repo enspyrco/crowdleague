6062e026c6bd60e62a244f7edf00a61eff0405f2
Nick Meinhold <nick.meinhold@gmail.com>
2020-06-01 15:21:59 +1000

    Fix results output in removeProfilePicFiles
    - fix output of delete results by iterating over imageSizes
    with for/of rather than forEach (removes function calls)
    - wrap all delete calls in try/catch and check error
    - only add failures to db if not a 404 (as functions can be
    called more than once)

--- functions/src/profile/on_delete_profile_pic/logs.ts
+++ functions/src/profile/on_delete_profile_pic/logs.ts
@@ -1,5 +1,4 @@
 import config from "./config";
-import { DeleteFileResponse } from 'firebase-admin/node_modules/@google-cloud/storage';
 
 export const start = () => {
   console.log("Started execution with configuration", config);
@@ -9,119 +8,18 @@ export const constructedFilePath = (path: string) => {
   console.log(`Constructed file path: '${path}'`);
 };
 
-// export const removingSizes = (sizesMap: Map<string, string>) => {
-//   console.log('Sizes are: ');
-//   for(const key in sizesMap.keys()) {
-//       console.log(`${key} : ${sizesMap.get(key)}, `);
-//   }
-// }
+export const deletionFailed = (filePath: string, error: any) => {
+  console.error(`Deleting ${filePath} returned: ${error}`);
+}
 
-export const deleteFileResponse = (identifier: string, response : DeleteFileResponse) => {
-    console.log(`deleted ${identifier} with response: ${response}`);
+export const deletedFile = (identifier: string) => {
+  console.log(`Successfully deleted ${identifier}`);
 }
 
 export const complete = () => {
   console.log("Completed removing profile pic files");
 };
 
-export const failed = (failures: any) => {
-  console.error("Failed removing profile pic files: "+failures);
+export const failed = (numFailures: number, userId: string, picId: string) => {
+  console.error(`There were ${numFailures} failures deleting profile pic files, added to "leaguers/${userId}/failed_profile_pics/${picId}"`);
 };
-
-// export const objectNameUndefined = () => {
-//   console.log("Object name was undefined");
-// }
-
-// export const addProfilePicsToFirestore = () => {
-//   console.log('Adding profile pic data to Firestore...');
-// }
-
-// export const addedProfilePicsToFirestore = () => {
-//   console.log('Added profile pic data to Firestore.');
-// }
-
-// export const errorAddingProfilePicsToFirestore = (err: Error)  => {
-//   console.error("Error when adding profile pic data to Firestore", err);
-// }
-
-// export const noContentType = () => {
-//   console.log(`File has no Content-Type, no processing is required`);
-// };
-
-// export const contentTypeInvalid = (contentType: string) => {
-//   console.log(
-//     `File of type '${contentType}' is not an image, no processing is required`
-//   );
-// };
-
-// export const error = (err: Error) => {
-//   console.error("Error when resizing image", err);
-// };
-
-// export const errorDeleting = (err: Error) => {
-//   console.warn("Error when deleting temporary files", err);
-// };
-
-// export const imageAlreadyResized = () => {
-//   console.log("File is already a resized image, no processing is required");
-// };
-
-// export const imageDownloaded = (remotePath: string, localPath: string) => {
-//   console.log(`Downloaded image file: '${remotePath}' to '${localPath}'`);
-// };
-
-// export const imageDownloading = (path: string) => {
-//   console.log(`Downloading image file: '${path}'`);
-// };
-
-// export const imageResized = (path: string) => {
-//   console.log(`Resized image created at '${path}'`);
-// };
-
-// export const imageResizing = (path: string, size: string) => {
-//   console.log(`Resizing image at path '${path}' to size: ${size}`);
-// };
-
-// export const imageUploaded = (path: string) => {
-//   console.log(`Uploaded resized image to '${path}'`);
-// };
-
-// export const imageUploading = (path: string) => {
-//   console.log(`Uploading resized image to '${path}'`);
-// };
-
-// export const init = () => {
-//   console.log("Initializing extension with configuration", config);
-// };
-
-// export const tempDirectoryCreated = (directory: string) => {
-//   console.log(`Created temporary directory: '${directory}'`);
-// };
-
-// export const tempDirectoryCreating = (directory: string) => {
-//   console.log(`Creating temporary directory: '${directory}'`);
-// };
-
-// export const tempOriginalFileDeleted = (path: string) => {
-//   console.log(`Deleted temporary original file: '${path}'`);
-// };
-
-// export const tempOriginalFileDeleting = (path: string) => {
-//   console.log(`Deleting temporary original file: '${path}'`);
-// };
-
-// export const tempResizedFileDeleted = (path: string) => {
-//   console.log(`Deleted temporary resized file: '${path}'`);
-// };
-
-// export const tempResizedFileDeleting = (path: string) => {
-//   console.log(`Deleting temporary resized file: '${path}'`);
-// };
-
-// export const remoteFileDeleted = (path: string) => {
-//   console.log(`Deleted original file from storage bucket: '${path}'`);
-// };
-
-// export const remoteFileDeleting = (path: string) => {
-//   console.log(`Deleting original file from storage bucket: '${path}'`);
-// };
--- functions/src/profile/on_delete_profile_pic/on_delete_profile_pic.ts
+++ functions/src/profile/on_delete_profile_pic/on_delete_profile_pic.ts
@@ -1,6 +1,5 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
-import { DeleteFileResponse } from 'firebase-admin/node_modules/@google-cloud/storage';
 
 import config from './config';
 import * as logs from './logs';
@@ -9,11 +8,26 @@ import * as database from './database';
 export async function removeProfilePicFiles(snapshot : functions.firestore.DocumentSnapshot, context : functions.EventContext) {
 
     ///////////////////////////////////////////////////////////////////////
-    // remove the files (original and resized)
+    // Remove all files (original and resized) for a profile pic. 
     // 
-    // see: 
+    // Any failures (other than 404) are added to the db at: 
+    //  -> "leaguers/${userId}/failed_profile_pics/${picId}"
+    //
+    // 404 errors are not considered failures as cloud functions can be called more than once 
+    // so we need to allow for subsequent calls where files are already deleted 
+    // 
+    // Relevant references: 
     //  - https://firebase.google.com/docs/reference/admin/node 
     //  - https://googleapis.dev/nodejs/storage/latest/File.html#delete 
+    //  
+    //  Each call to delete() returns a DeleteBucketResponse, an array with an object at 
+    //  position 0 that holds the full API response, but apparently the response is empty 
+    //  if successful. 
+    //  - https://googleapis.dev/nodejs/storage/latest/global.html#DeleteFileResponse
+    //  - https://cloud.google.com/storage/docs/json_api/v1/objects/delete 
+    //
+    //  When using promises and await, errors need to be caught. Error responses are described here: 
+    //  - https://cloud.google.com/storage/docs/json_api/v1/status-codes 
     //  
     ///////////////////////////////////////////////////////////////////////
 
@@ -30,26 +44,39 @@ export async function removeProfilePicFiles(snapshot : functions.firestore.Docum
         const failures : any = {};
         let numFailures = 0;
         // delete original 
-        const originalResponse : DeleteFileResponse = await bucket.file(filePath).delete();
-        logs.deleteFileResponse('original', originalResponse);
-        if(originalResponse[0].success === false) {
-            failures['original'] = originalResponse;
-            numFailures++;
+        try {
+            await bucket.file(filePath).delete();
+            logs.deletedFile(filePath);
         }
+        catch(error) {
+            logs.deletionFailed(filePath, error);
+            if(error.code !== 404) { // if the file is not there, no need to add failure to db
+                failures['original'] = JSON.parse(JSON.stringify(error));
+                numFailures++;
+            }
+        }
+        
 
         // Convert to a set to remove any duplicate sizes
         const imageSizes = new Set(config.imageSizes);
-        imageSizes.forEach(async (size) => {
-            const sizeResponse : DeleteFileResponse = await bucket.file(filePath+'_'+size).delete();
-            logs.deleteFileResponse(size, sizeResponse);
-            if(sizeResponse[0].success === false) {
-                failures[size] = sizeResponse;
-                numFailures++;
+        // Iterate over the set and attempt to delete each image 
+        for (const imageSize of imageSizes) {
+            const resizedFilePath = filePath+'_'+imageSize;
+            try {
+                await bucket.file(resizedFilePath).delete();
+                logs.deletedFile(resizedFilePath);
             }
-        });
+            catch(error) {
+                logs.deletionFailed(resizedFilePath, error);
+                if(error.code !== 404) { // if the file is not there, no need to add failure to db
+                    failures[imageSize] = JSON.parse(JSON.stringify(error));
+                    numFailures++;
+                }
+            }
+        }
 
         if (numFailures > 0) {
-            logs.failed(failures);
+            logs.failed(numFailures, context.params.userId, context.params.picId);
             await db.failed(failures);
             return;
         }
@@ -57,7 +84,6 @@ export async function removeProfilePicFiles(snapshot : functions.firestore.Docum
         
     }
     catch(error) {
-        console.error(error);
-        
+        console.error('Uncaught exception: '+error);
     }
 }
