bf227fc2197ddc6cac7aa6e3632737b4fe24087f
Nick Meinhold <nick.meinhold@gmail.com>
2020-10-14 03:50:25 +1100

    Clean up

--- .scripts/lcov.sh
+++
@@ -1,8 +0,0 @@
-# taken from https://github.com/actions/virtual-environments/issues/560#issuecomment-602543882 
-VERSION="1.14"
-mkdir Iconv
-cd Iconv
-wget "https://github.com/linux-test-project/lcov/releases/download/v1.14/lcov-$VERSION.tar.gz"
-tar -xzf "lcov-$VERSION.tar.gz"
-cd "lcov-$VERSION"
-sudo make install
