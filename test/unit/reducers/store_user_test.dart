5db0b21903a5feba377129143a7580c389d830b0
tamari-gray <tamari-gray>
2020-10-20 13:02:56 +1100

    store user reducer test :)

--- test/unit/reducers/store_user_test.dart
+++ test/unit/reducers/store_user_test.dart
@@ -1,27 +1,45 @@
-import 'package:crowdleague/actions/auth/clear_user_data.dart';
-import 'package:crowdleague/actions/device/store_platform.dart';
-import 'package:crowdleague/enums/device/platform_type.dart';
+import 'package:crowdleague/actions/auth/store_user.dart';
 import 'package:crowdleague/models/app/app_state.dart';
 import 'package:crowdleague/reducers/app_reducer.dart';
 import 'package:redux/redux.dart';
 import 'package:test/test.dart';
 
+import '../../mocks/models/user_mocks.dart';
+
 void main() {
   group('StoreUserReducer', () {
-    test('clears any user data from state', () {
-      // create a basic store with the app reducers
+    test('Adds new user to store', () {
+      // Init mocks
+      final testUser = mockUser;
+
+      // Create a basic store with the app reducers
       final store = Store<AppState>(
         appReducer,
         initialState: AppState.init(),
       );
 
-      // dispatch action to clear user data
-      store.dispatch(ClearUserData());
+      // Dispatch action
+      store.dispatch(StoreUser(user: testUser));
 
-      // check that the store has the expected value
-      // expect(store.state.systemInfo.platform, PlatformType.macOS);
+      // Check that the store has the expected value
+      expect(store.state.user, testUser);
+    });
+    test('Resets navigation stack', () {
+      // Init mocks
+      final testUser = mockUser;
 
-      //
+      // Create a basic store with the app reducers
+      final initialAppState = AppState.init();
+      final store = Store<AppState>(
+        appReducer,
+        initialState: initialAppState,
+      );
+
+      // Dispatch action
+      store.dispatch(StoreUser(user: testUser));
+
+      // Check that the store has the expected value
+      expect(store.state.pagesData, initialAppState.pagesData);
     });
   });
 }
