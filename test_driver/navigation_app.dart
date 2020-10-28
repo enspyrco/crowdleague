8e8f9ebf1221a6c8b5cf4b2d63434bb6e55500e4
Nick Meinhold <nick.meinhold@gmail.com>
2020-10-28 13:30:36 +1100

    Move files into folders or delete (#265)

--- test_driver/navigation_app.dart
+++
@@ -1,8 +0,0 @@
-import 'package:crowdleague/widgets/app/crowd_league_app.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_driver/driver_extension.dart';
-
-void main() {
-  enableFlutterDriverExtension();
-  runApp(CrowdLeagueApp());
-}
