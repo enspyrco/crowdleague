b85a8390fd71bfd6e1c1a9af12224a9ab17c7d2b
Trevor Davis <tzendou@gmail.com>
2020-11-04 19:10:59 -0800

    Refactor Problem class

--- lib/models/problems/problem.dart
+++ lib/models/problems/problem.dart
@@ -5,30 +5,22 @@ import 'dart:convert';
 import 'package:built_collection/built_collection.dart';
 import 'package:built_value/built_value.dart';
 import 'package:built_value/serializer.dart';
-import 'package:crowdleague/enums/problem_type.dart';
-import 'package:crowdleague/models/app/app_state.dart';
 import 'package:crowdleague/utils/serializers.dart';
 import 'package:meta/meta.dart';
 
 part 'problem.g.dart';
 
 abstract class Problem implements Built<Problem, ProblemBuilder> {
-  ProblemType get type;
-  String get message;
+  String get errorMessage;
+  String get stackTrace;
   BuiltMap<String, Object> get info;
-  @nullable
-  String get trace;
-  @nullable
-  AppState get state;
 
   Problem._();
 
   factory Problem(
-      {@required ProblemType type,
-      @required String message,
-      BuiltMap<String, Object> info,
-      String trace,
-      AppState state}) = _$Problem._;
+      {@required String errorMessage,
+      @required String stackTrace,
+      BuiltMap<String, Object> info}) = _$Problem._;
 
   factory Problem.by([void Function(ProblemBuilder) updates]) = _$Problem;
 
