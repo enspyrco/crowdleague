47cc291d61e581ce32c9cd23d43a2356961a558f
tamari-gray <tamari-gray>
2020-12-23 15:14:46 +1100

    hoot

---
+++ test/sections/venues/UC1/reducer/store_nav_bar_selection_reducer_test.dart
@@ -0,0 +1,25 @@
+import 'package:crowdleague/actions/navigation/store_nav_bar_selection.dart';
+import 'package:crowdleague/enums/navigation/nav_bar_selection.dart';
+import 'package:crowdleague/models/app/app_state.dart';
+import 'package:crowdleague/reducers/navigation/store_nav_bar_selection.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('StoreNavBarSelection', () {
+    test('sets AppState.navBarSelection to venues', () {
+      // Setup an initial app state
+      final initialState = AppState.init();
+      expect(initialState.navBarSelection.name, 'home');
+
+      // Create the reducer.
+      final rut = StoreNavBarSelectionReducer();
+
+      // Invoke the reducer to get a new state.
+      final newState = rut.reducer(initialState,
+          StoreNavBarSelection(selection: NavBarSelection.valueOfIndex(1)));
+
+      // Check that the new state has the new navbarSelection.
+      expect(newState.navBarSelection.name, 'venues');
+    });
+  });
+}
