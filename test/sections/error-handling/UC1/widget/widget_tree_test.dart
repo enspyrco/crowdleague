7db5aa0a9673c0c7943b699acb2f17a0c8e55aab
Trevor Davis <tzendou@gmail.com>
2020-12-27 22:41:44 -0700

    Create test

---
+++ test/sections/error-handling/UC1/widget/widget_tree_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('ProblemPage', () {
+    testWidgets('AddProblem results in expected widget tree',
+        (WidgetTester tester) async {});
+  });
+}
