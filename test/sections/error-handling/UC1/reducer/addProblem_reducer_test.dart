0cfb57cdaf2c2e95b9c1e0ba94aa3bac78902a88
Trevor Davis <tzendou@gmail.com>
2020-12-27 21:40:17 -0700

    Revise reducer test

--- test/sections/error-handling/UC1/reducer/addProblem_reducer_test.dart
+++ test/sections/error-handling/UC1/reducer/addProblem_reducer_test.dart
@@ -1,30 +1,34 @@
-import 'package:crowdleague/actions/navigation/store_nav_bar_selection.dart';
-import 'package:crowdleague/enums/navigation/nav_bar_selection.dart';
+import 'package:crowdleague/actions/navigation/add_problem.dart';
 import 'package:crowdleague/models/app/app_state.dart';
+import 'package:crowdleague/models/navigation/page_data/problem_page_data.dart';
+import 'package:crowdleague/models/problems/general_problem.dart';
 import 'package:crowdleague/reducers/app_reducer.dart';
-import 'package:crowdleague/reducers/navigation/store_nav_bar_selection.dart';
+import 'package:crowdleague/utils/problem_utils.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('AddProblemReducer', () {
-    test('Adds Problem to appState.problems and ProblemPageData to appState.pagesData', () {
-      // Setup initial app state.
+    test(
+        'Adds Problem to appState.problems and ProblemPageData to appState.pagesData',
+        () {
+      // Setsup the initial app state.
       final initialState = AppState.init();
-      expect(initialState.navBarSelection.name, 'home');
+      
+      expect(initialState.problems.length, 0);
+      expect(initialState.pagesData.length, 1);
 
-      // Create the reducer to test.
       final reducer = AddProblemReducer();
+      final problem = createProblem(
+          GeneralProblem, 'Problem error message', StackTrace.current);
 
-      // Invoke the reducer to rebuild appState.
-      final newState = reducer.reducer(
-        initialState,
-        AddProblemReducer(
-          
-        ),
-      );
+      // Invokes the reducer to rebuild appState.
+      final newState =
+          reducer.reducer(initialState, AddProblem(problem: problem));
 
-      // Check that the new state has the new navbarSelection.
-      expect(newState.navBarSelection.name, 'venues');
+      expect(newState.problems.first, problem);
+      expect(newState.pagesData.length, 2);
+      expect((newState.pagesData[1] as ProblemPageData).problem.message,
+          problem.message);
     });
   });
 }
