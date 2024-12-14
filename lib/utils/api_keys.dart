ee9172de1312b3ea440d623462678825987bb8c3
Nick Meinhold <nick.meinhold@gmail.com>
2024-12-15 08:38:16 +1100

    Reverse geocode latlng to autofill Venue address (#58)

---
+++ lib/utils/api_keys.dart
@@ -0,0 +1,7 @@
+// if we ever make this repo public we will need to regenerate the keys and gitignore this file
+//
+// We will also  need to find a way to use the keys in
+// android/app/src/main/AndroidManifest.xml and ios/Runner/AppDelegate.swift
+// without including the keys in the repo.
+
+const String reverseGeocodingApiKey = 'AIzaSyCWkWFbUqnCHJ1NGVeKM_urOz_4N-ezvrM';
