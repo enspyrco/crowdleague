637b91271d9f444013d5bb39d9b22123c9269056
Nick Meinhold <nick@enspyr.co>
2024-12-17 12:11:22 +1100

    claude plus some error fixing

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: ["tsconfig.json", "tsconfig.dev.json"],
+    project: ["./functions/tsconfig.json", "./functions/tsconfig.dev.json"],
     sourceType: "module",
   },
   ignorePatterns: [
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,14 +6,71 @@
  *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
+import * as admin from "firebase-admin";
+import {
+  onDocumentCreated,
+} from "firebase-functions/v2/firestore";
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+// Initialize Firebase Admin SDK
+admin.initializeApp();
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+exports.notifyFriendsOnNewCheckin = onDocumentCreated(
+  "checkins/{venueId}", async (event) => {
+    // Get the new checkin data
+    const {params, data} = event;
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+    // Safely extract entry data
+    if (!data) {
+      console.log("No data in the event");
+      return;
+    }
+
+    const newCheckin = data.data();
+
+    const venueId = params.venueId;
+
+    try {
+      // Fetch the author's friends
+      const userRef = admin.firestore().collection("users").doc(newCheckin.uid);
+      const userDoc = await userRef.get();
+      const friendIds = userDoc.data()?.friends || [];
+
+      // Batch notifications with more efficient processing
+      const notificationTasks = friendIds.map(async (friendId: string) => {
+        // Fetch friend's token
+        const tokenDoc = await admin.firestore()
+          .collection("user_tokens")
+          .doc(friendId)
+          .get();
+
+        const friendToken = tokenDoc.data()?.token;
+
+        if (friendToken) {
+          const message = {
+            notification: {
+              title: "New Checkin",
+              body: "Your friend added a new entry!",
+            },
+            token: friendToken,
+            data: {
+              type: "new_entry",
+              venueId: venueId,
+              checkingInId: newCheckin.uid,
+            },
+          };
+
+          try {
+            await admin.messaging().send(message);
+            console.log(`Notification sent to friend: ${friendId}`);
+          } catch (error) {
+            console.error("Messaging error:", error);
+          }
+        }
+      });
+
+      // Wait for all notifications to complete
+      await Promise.all(notificationTasks);
+    } catch (error) {
+      console.error("Function execution error:", error);
+    }
+  });
