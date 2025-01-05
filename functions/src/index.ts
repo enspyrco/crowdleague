941bdc9fc28a84f62f7d825a2e98e1feb21e4eb6
Nick Meinhold <nick@enspyr.co>
2025-01-05 17:34:24 +1100

    rename follow to crew

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,6 @@ import * as admin from 'firebase-admin';
 
 admin.initializeApp();
 
-export * from './follow-request';
-export * from './accept-follow-request';
+export * from './crew-request';
+export * from './accept-crew-request';
 export * from './follow-back';
