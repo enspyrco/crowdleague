5dca8f444013effcffd226149ba65c2c12b74877
Nick Meinhold <nick@enspyr.co>
2025-01-15 19:39:36 +1100

    Return a promise in updateTokensEverywhere functions
    updateTokensEverywhere returns a Promise but we weren't returning it
    in the calling function.

--- functions/src/update-tokens-everywhere.ts
+++ functions/src/update-tokens-everywhere.ts
@@ -12,7 +12,7 @@ export const updateTokensEverywhereAus = onDocumentUpdated({
   region: 'australia-southeast2',
 },
 async (event) => {
-  updateTokensEverywhere(event, 'australia-southeast2');
+  return updateTokensEverywhere(event, 'australia-southeast2');
 });
 
 export const updateTokensEverywhereUsa = onDocumentUpdated({
@@ -21,7 +21,7 @@ export const updateTokensEverywhereUsa = onDocumentUpdated({
   region: 'us-central1',
 },
 async (event) => {
-  updateTokensEverywhere(event, '(default)');
+  return updateTokensEverywhere(event, '(default)');
 });
 
 /**
@@ -29,7 +29,7 @@ async (event) => {
  * @param {FirestoreEvent<Change<QueryDocumentSnapshot> |
  *    undefined, {playerId: string}>} event
  * @param {string} dbName,
- * @return {Promise}
+ * @return {Promise<null>}
  */
 async function updateTokensEverywhere(
   event : FirestoreEvent<Change<QueryDocumentSnapshot> | undefined,
@@ -62,19 +62,22 @@ async function updateTokensEverywhere(
     if (snapshot.empty) {
       console.log('No documents found containing the old token');
       return null;
+    } else {
+      console.log('Got a snapshot of followers that have the old token.');
     }
 
     // Batch updates for better performance and atomicity
     const batch = getFirestore(dbName).batch();
+    console.log('Got a batch object from firestore.');
 
     for (const doc of snapshot.docs) {
       // Update the token in the list
       batch.update(doc.ref, {followerTokens: FieldValue.arrayRemove(oldToken)});
       batch.update(doc.ref, {followerTokens: FieldValue.arrayUnion(newToken)});
     }
+    console.log('Added updates to batch.');
 
     await batch.commit();
-
     console.log(`Successfully updated ${snapshot.size} documents`);
   } catch (error) {
     console.error('Error updating tokens:', error);
