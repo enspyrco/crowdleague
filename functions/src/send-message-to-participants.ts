d6df065664f3db60ae6b83e5e7dcf350b160b8c1
Nick Meinhold <nick@enspyr.co>
2025-01-31 13:11:43 +1100

    Remove return from callable callable function
    `sendMessageToParticipants` was returning an object with the results of
    sending FCM messages.
    
    I'm not sure if we just shouldn't return anything or if there was a bug
    in the code but we don't use the returned value anyway.

--- functions/src/send-message-to-participants.ts
+++ functions/src/send-message-to-participants.ts
@@ -92,18 +92,6 @@ export const sendMessageToParticipants = onCall(
       // Send the message
       const response = await messaging().sendEachForMulticast(message);
       log(`Sent the message, response: ${response}`);
-
-      // we were getting a 409 error in the logs but all logs in the code were
-      // being printed so I've commented out the return statement.
-      // // Return the response
-      // return {
-      //   successCount: response.successCount,
-      //   failureCount: response.failureCount,
-      //   failedTokens: response.responses
-      //     .map((resp, idx) => (!resp.success ?
-      //       conversationData.participantTokens[idx] : null))
-      //     .filter((token): token is string => token !== null),
-      // };
     } catch (e) {
       throw new HttpsError('aborted', `${e}.`);
     }
