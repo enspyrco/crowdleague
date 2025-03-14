aa9e7fcb25ffc6777d554ca99c2d3d9d98b7cde5
Nick Meinhold <nick@enspyr.co>
2025-03-14 12:46:39 +1100

    Have 2 separate resize-images functions
    The resize-images functions run on our different buckets - a
    bucket in the us region and one in the aus region.
    
    We also write to the appropriate firestore database, ie. we use the
    firestore in the same region as the bucket and function.

--- functions/src/resize-images.ts
+++ functions/src/resize-images.ts
@@ -79,71 +79,92 @@ async function processImage(
   return Promise.all(resizePromises);
 }
 
+/**
+ * Save resized images
+ * @constructor
+ * @param {StorageEvent} event - The event that triggered the function
+ * @param {string} databaseName - The database the function accesses
+ */
+async function saveImages(event: StorageEvent, databaseName: string) {
+  const bucketName = event.data.bucket;
+  const filePath = event.data.name;
+  const fileName = path.basename(filePath);
+  const contentType = event.data.contentType;
+  const folders = path.dirname(filePath).split(path.sep);
+  const area = folders[0]; // venues or profiles
+  const areaId = folders[1]; // venueId or profileId
+  const timestamp = fileName.split('.')[0];
+
+  // Exit if this is triggered by a resized image
+  if (SIZES.some((size) => fileName.includes(`_${size.suffix}`))) {
+    logger.log('Skipping already resized image:', fileName);
+    return null;
+  }
+
+  // Exit if this is not an image
+  if (!contentType?.startsWith('image/')) {
+    logger.log('This is not an image:', fileName);
+    return null;
+  }
+
+  try {
+    const firestore = getFirestore(databaseName);
+
+    // Check if this is an update or new file
+    const isUpdate = event.data.metageneration > 1;
+    logger.log(`Processing ${isUpdate ? 'update to' : 'new'} image:`,
+      fileName);
+
+    const storage = new Storage();
+    const bucket = storage.bucket(bucketName);
+    const file = bucket.file(filePath);
+
+    // Update the status of the profile pic processing
+    await firestore.collection(area).doc(areaId).set({
+      picStatus: 'processing'}, {merge: true}
+    );
+
+    // Process the image
+    const results = await processImage(bucket, file, filePath, contentType);
+
+    logger.log(`Saved image with name (timestamp): ${timestamp} ` +
+      `to ${area}/${areaId}/`);
+
+    // Set the picId and add update the status of the profile pic processing.
+    await firestore.collection(area).doc(areaId).set({
+      picId: +timestamp, picStatus: 'complete'}, {merge: true}
+    );
+    // Add the picId to the picIds array, which keeps track of uploaded
+    // profile pics.
+    await firestore.collection(area).doc(areaId).update({
+      picIds: FieldValue.arrayUnion(+timestamp),
+    });
+
+    return results;
+  } catch (error) {
+    logger.error('Error processing image:',
+      error instanceof Error ? error.message : 'Unknown error');
+    throw error;
+  }
+}
+
 // The upload file is assumed to have form `bucketName/userId/timestamp.jpg`
-export const resizeImages = functions.storage.onObjectFinalized(
-  // {
-  //   bucket: 'myBucket',
-  //   region: 'asia-northeast1',
-  // },
+export const resizeImagesAus = functions.storage.onObjectFinalized(
+  {
+    bucket: 'crowdleague-project-aus',
+    region: 'australia-southeast1',
+  },
   async (event: StorageEvent) => {
-    const bucketName = event.data.bucket;
-    const filePath = event.data.name;
-    const fileName = path.basename(filePath);
-    const contentType = event.data.contentType;
-    const folders = path.dirname(filePath).split(path.sep);
-    const area = folders[0]; // venues or profiles
-    const areaId = folders[1]; // venueId or profileId
-    const timestamp = fileName.split('.')[0];
-
-    // Exit if this is triggered by a resized image
-    if (SIZES.some((size) => fileName.includes(`_${size.suffix}`))) {
-      logger.log('Skipping already resized image:', fileName);
-      return null;
-    }
-
-    // Exit if this is not an image
-    if (!contentType?.startsWith('image/')) {
-      logger.log('This is not an image:', fileName);
-      return null;
-    }
-
-    try {
-      const firestore = getFirestore('firestore-usa');
-
-      // Check if this is an update or new file
-      const isUpdate = event.data.metageneration > 1;
-      logger.log(`Processing ${isUpdate ? 'update to' : 'new'} image:`,
-        fileName);
-
-      const storage = new Storage();
-      const bucket = storage.bucket(bucketName);
-      const file = bucket.file(filePath);
-
-      // Update the status of the profile pic processing
-      await firestore.collection(area).doc(areaId).set({
-        picStatus: 'processing'}, {merge: true}
-      );
-
-      // Process the image
-      const results = await processImage(bucket, file, filePath, contentType);
-
-      logger.log(`Saved image with name (timestamp): ${timestamp} ` +
-        `to ${area}/${areaId}/`);
-
-      // Set the picId and add update the status of the profile pic processing.
-      await firestore.collection(area).doc(areaId).set({
-        picId: +timestamp, picStatus: 'complete'}, {merge: true}
-      );
-      // Add the picId to the picIds array, which keeps track of uploaded
-      // profile pics.
-      await firestore.collection(area).doc(areaId).update({
-        picIds: FieldValue.arrayUnion(+timestamp),
-      });
-
-      return results;
-    } catch (error) {
-      logger.error('Error processing image:',
-        error instanceof Error ? error.message : 'Unknown error');
-      throw error;
-    }
+    saveImages(event, '(default)');
   });
+
+// The upload file is assumed to have form `bucketName/userId/timestamp.jpg`
+export const resizeImagesUsa = functions.storage.onObjectFinalized(
+  {
+    bucket: 'crowdleague-project.firebasestorage.app',
+    region: 'us-central1',
+  },
+  async (event: StorageEvent) => {
+    saveImages(event, 'firestore-usa');
+  });
+
